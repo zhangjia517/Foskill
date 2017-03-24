dce7f80edff1e2bfd47f0f20c607e43966dee428
Foskill <zhangjia517@hotmail.com>
2017-03-24 20:28:26 +0800

    Del Some Asset

---
+++ Assets/Script/DKAnimator/DKAnimatorCtrl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class DKAnimatorCtrl : MonoBehaviour
+{
+    protected Animator PlayerAnimator;
+
+    public void Start()
+    {
+        PlayerAnimator = GetComponent<Animator>();
+
+        if (PlayerAnimator.layerCount >= 2)
+            PlayerAnimator.SetLayerWeight(1, 1);
+    }
+
+    public void Update()
+    {
+        if (PlayerAnimator)
+        {
+//            AnimatorStateInfo stateInfo = PlayerAnimator.GetCurrentAnimatorStateInfo(0);
+
+            if (Input.GetButtonDown("Attack1")) PlayerAnimator.SetBool("Attack1", true);
+            if (Input.GetButtonUp("Attack1")) PlayerAnimator.SetBool("Attack1", false);
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                PlayerAnimator.SetBool("Dodge", true);
+            }
+
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            PlayerAnimator.SetFloat("Speed", h * h + v * v);
+        }
+    }
+}
